49dc52843a71689119d7542bcf7b57810272c318
zhek1chan <evg.ermolov@yandex.ru>
2024-04-01 15:48:15 +0500

    added many

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/fragment/industry/FiltersIndustryFragment.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.presentation.filters.fragment.industry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFilterChooseIndustryBinding
+
+class FiltersIndustryFragment : Fragment() {
+    private var _binding: FragmentFilterChooseIndustryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentFilterChooseIndustryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/fragment/main/FiltersFragment.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.presentation.filters.fragment.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFiltersBinding
+
+class FiltersFragment : Fragment() {
+
+    private var _binding: FragmentFiltersBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFiltersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

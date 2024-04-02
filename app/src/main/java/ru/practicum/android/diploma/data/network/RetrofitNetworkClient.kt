81445f9305a14697685faecc455134548fc19e58
artsa2 <Artem.Salnikov@sitronics-kt.ru>
2024-04-03 00:16:21 +0300

    Added retrofit client

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -0,0 +1,34 @@
+package ru.practicum.android.diploma.data.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import ru.practicum.android.diploma.common.Constants
+import ru.practicum.android.diploma.data.dto.Response
+
+class RetrofitNetworkClient(
+    private val context : Context,
+    private val hhService : HhApi
+) : NetworkClient {
+    override suspend fun doRequest(dto: Any): Response {
+        try {
+            if (!isConnected()) {
+                return Response().apply { resultCode = Constants.NO_CONNECTION }
+            }
+            return Response().apply { resultCode = Constants.BAD_REQUEST }
+        }
+        catch (e : Exception){
+            return Response().apply { resultCode=Constants.BAD_REQUEST }
+        }
+    }
+
+    private fun isConnected(): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+
+        return capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+    }
+
+}

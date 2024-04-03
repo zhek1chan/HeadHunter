b75263259eebd23a42f48bcf82bc8356bf87b388
zhek1chan <evg.ermolov@yandex.ru>
2024-04-04 00:32:03 +0500

    detekt fixes

--- app/src/main/java/ru/practicum/android/diploma/presentation/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/root/RootActivity.kt
@@ -10,10 +10,12 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRootBinding.inflate(layoutInflater)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment

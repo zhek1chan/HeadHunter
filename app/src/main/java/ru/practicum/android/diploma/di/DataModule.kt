de46323adce2e1338714c9f644598ebe18cdde81
zhek1chan <evg.ermolov@yandex.ru>
2024-04-03 16:24:21 +0500

    fix

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,19 +1,28 @@
 package ru.practicum.android.diploma.di
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import ru.practicum.android.diploma.common.Constants
+import ru.practicum.android.diploma.data.Constant
 import ru.practicum.android.diploma.data.network.HhApi
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
 
 val dataModule = module {
-    single<HhApi>{
+    single<HhApi> {
         Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
+            .baseUrl(Constant.HH_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .apply {
+                        addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                    }
+                    .build()
+            )
             .build()
             .create(HhApi::class.java)
     }

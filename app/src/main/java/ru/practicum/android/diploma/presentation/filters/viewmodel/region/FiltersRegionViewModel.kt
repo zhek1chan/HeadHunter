47ddd511013bb386aeed857e866b44fad0225401
zhek1chan <evg.ermolov@yandex.ru>
2024-04-22 12:34:25 +0500

    detekt

--- app/src/main/java/ru/practicum/android/diploma/presentation/filters/viewmodel/region/FiltersRegionViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filters/viewmodel/region/FiltersRegionViewModel.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.presentation.filters.viewmodel.region
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
